2a7cea929b77314be236b635a3d4d4f9de016f5e
Kulwant Olkha <kulwantolkha@gmail.com>
2025-09-11 17:51:43 +0530

    Enhance owner dashboard with data display and recent bookings overview

--- frontend/src/components/owner/Sidebar.jsx
+++ frontend/src/components/owner/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
   return (
     <>
       <div className="relative min-h-screen md:flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-borderColor text-sm">
-        <div className="group relative">
+        <div className="group relative h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto">
           <label htmlFor="image">
             <img
               src={
@@ -21,7 +21,9 @@ const Sidebar = () => {
                   : user?.image ||
                     "https://unsplash.com/photos/a-white-percentage-sign-logo-surrounded-by-green-leaves-HDuabVTejQc"
               }
+              className="h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto"
             />
+            <input type="file" id="image" accept="image/*" hidden onChange= {(e) => setImage(e.target.files[0])}/>
 
             <div className="absolute hidden top-0 right-0 left-0 bottom-0 bg-black/10 rounded-full group-hover:flex items-center justify-center cursor-pointer">
               <img src={assets.edit_icon} />
